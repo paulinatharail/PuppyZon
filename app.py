2ca99b868fb37f1ebce69dd0b97b77ea25c0470f
Fangqing Yu <francis.fq.yu@gmail.com>
2019-03-23 11:54:10 -0700

    place holder

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
 #Flask App
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print(request)
+
+        if request.files.get('file'):
+            # read the file
+            file = request.files['file']
+
+            # read the filename
+            filename = file.filename
+
+            # Save the file to the uploads folder
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "Image Saved!"
+
