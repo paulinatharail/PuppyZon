ca3ca3f31ba5f1d4119eff958c0cf818bd6cffaf
paulina <paulinatharail@yahoo.com>
2019-03-24 10:44:05 -0700

    Fixed JS user input

--- static/js/main.js
+++ static/js/main.js
@@ -1,25 +1,60 @@
-// javascript
-var animalType = d3.select("#animalType");
 
-var animalType_value = animalType.property("value");
+//javascript
 
-var BreedName = d3.select("#breedName").property("value");
-var zip = d3.select("#zipcode").property("value");
-console.log("Animal Type: " + animalType_value + " Breed Name: " + BreedName + "Zip code: " +zip );   
+var findPetButton = d3.select('#FindPet');
 
 
-function startRead(evt) {
-    var file = document.getElementById('file').files[0];
-    if (file) {
-        alert("Name: " + file.name + "\n" + "Last Modified Date :" + file.lastModifiedDate);
+//on Button click event
+findPetButton.on("click", function(){
+    
+    //Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    //Get the user input data
+    var animalType = d3.select('#animalType').property("value");
+   
+    //BreedName
+    var breedName = d3.select('#BreedName').property("value");
+    //zipcode
+    var zipcode = d3.select('#zipcode').property("value");
+    
+    console.log("Animal Type is " + animalType);
+    console.log("Breed Type is " + breedName);
+    console.log("zipcode is " + zipcode);
+
+    //Call the API with these 3 parameters
+
+});
+
+
+//on Button click event
+var browse = d3.select('#browse');
+var fileBrowse = d3.select('#file');
+var fileUploadDIV = d3.select('#fileUpload');
+
+browse.on("click", function(){
+    console.log("Browse button clicked");
+
+    
+
+
+
+    var fileSelector = document.createElement('input');
+    fileSelector.setAttribute('type', 'file');
+    fileSelector.setAttribute('id', 'fileSelector');
+
+    var selectDialogLink = document.createElement('a');
+    selectDialogLink.setAttribute('href', '');
+    selectDialogLink.setAttribute('id', 'fileSelector');
+
+    selectDialogLink.innerText = 'Select File';
+
+    selectDialogLink.onclick = function() {
+        fileSelector.click();
+        return false;
     }
-}
 
-function readFormData(evt) {
+    // document.body.appendChild(selectDialogLink);
+    fileUploadDIV.append("fileSelector");
+});
 
-    alert("Animal Type: " + animalType_value + " Breed Name: " + BreedName + "Zip code: " +zip );  
-}
-
-
-startRead();
-readFormData(animalType_value, BreedName, zip);
