16a848d00c8e6c0bd3730600608f57ec7d3b0e18
Fangqing Yu <francis.fq.yu@gmail.com>
2019-03-30 19:11:44 -0700

    model added to flask 3/30/19

--- app.py
+++ app.py
@@ -3,50 +3,16 @@ import os
 import io
 import numpy as np
 import pandas as pd
-
-# import keras
-# from keras.preprocessing import image
-# from keras.preprocessing.image import img_to_array
-# from keras.applications.xception import (
-#     Xception, preprocess_input, decode_predictions)
-# from keras import backend as K
+from VGG19_model import predict
 
 from flask import Flask, request, render_template, redirect, url_for, jsonify
-
-#import PetFinder
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 
-#model = None
-
-# # Load trained model
-# def load_model():
-#     global model
-#     model = " "
-
-# load_model()
-
-# # Resize image for model using keras
-# def prepare_image(img):
-#     img = img_to_array(img)
-#     img = np.expand_dims(img, axis=0)
-#     img = preprocess_input(img)
-#     # return the processed image
-#     return img
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
-
-    # Plan A: make the API Calls
-    # =====================================================
-
-    # cats = PetFinder.petFinder("cat", "adoptable", 1000)
-    # dogs = PetFinder.petFinder("dog", "adoptable", 1000)
-    # results = pd.concat([cats, dogs], sort=None)
-    # results.to_csv("Resources/allpets.csv")
 
 
     # Plan B: use CSV
@@ -63,12 +29,15 @@ def upload_file():
 
             # read the filename
             filename = file.filename
+            
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             # Save the file to the uploads folder
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return "Image Saved!"
+            file.save(filepath)
+            breed = predict(filepath)
+            #render the homepage but with breed ifo incuded
+            return render_template("index2.html", breed=breed)
     
-    return render_template("index.html")
+    return render_template("index2.html")
             
             
 @app.route('/listings')
@@ -85,20 +54,22 @@ def listings():
     #Model returns animal type and breed type
     # Make API calls and store results in results 
     """ 
-        
+    
+    breedParam = request.args.get('breed')   
     results = pd.read_csv("Resources/allpets.csv")
     #print(results)
 
-    param = "pug" # test param
+    param = breedParam 
     breed = param.title() 
-    #print(breed)
+    
         
     filtered_results = results[results["primary breed"] == breed]
+    #filter by breed and state
+    #filtered_results = results[(results["primary breed"] == breed) & (results["state"] == "CA")]
     filtered_results = filtered_results.fillna("")
 
     #print(filtered_results["primary breed"])
 
-       
     filtered_results.to_csv('Resources/filtered_results.csv', header=True, index=False) 
         
     columns = list(filtered_results.columns)
